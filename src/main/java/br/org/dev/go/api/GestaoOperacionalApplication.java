e528e0d93d5e849cf909f4f5fb60939460ec19df
Eduardo Alex <duukaa@gmail.com>
2018-05-05 20:17:59 -0300

    Arquivos iniciais

---
+++ src/main/java/br/org/dev/go/api/GestaoOperacionalApplication.java
@@ -0,0 +1,12 @@
+package br.org.dev.go.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOperacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOperacionalApplication.class, args);
+	}
+}
